19e4df03d9b46a55f4e831c27defa888ac0f8a4d
uki7991 <tilek.kubanov@gmail.com>
2019-08-27 20:06:27 +0600

    tables

---
+++ app/Http/Controllers/CstatusController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Cstatus;
+use Illuminate\Http\Request;
+
+class CstatusController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Cstatus  $cstatus
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Cstatus $cstatus)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Cstatus  $cstatus
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Cstatus $cstatus)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Cstatus  $cstatus
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Cstatus $cstatus)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Cstatus  $cstatus
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Cstatus $cstatus)
+    {
+        //
+    }
+}
