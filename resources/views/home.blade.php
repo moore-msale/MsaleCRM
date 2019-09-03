14809e1404d4812920ddb7d01e2719e12a3afe47
uki7991 <tilek.kubanov@gmail.com>
2019-09-03 15:22:39 +0600

    registering call btn

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -18,58 +18,58 @@
 
         </div>
     </div>
-@include('modals.create_task')
-@include('modals.create_call')
-@include('modals.create_meet')
-@include('modals.create_client')
-@include('modals.called-modal')
+    @include('modals.create_task')
+    @include('modals.create_call')
+    @include('modals.create_meet')
+    @include('modals.create_client')
+    @include('modals.called-modal')
 @endsection
 
 @push('scripts')
-    @push('scripts')
-        <script>
+    <script>
+        $.ajax({
+            url: '{{ route('task.index') }}',
+            success: data => {
+                console.log(data);
+            },
+            error: () => {
+                console.log('error');
+            }
+        })
+    </script>
+    <script>
+        $('.addTask').click(e => {
+            e.preventDefault();
+            let btn = $(e.currentTarget);
+            let title = $('#taskname');
+            let desc = $('#taskdescription');
+            let date = $('#taskdate');
+            let user = $('#taskuser');
+
             $.ajax({
-                url: '{{ route('task.index') }}',
+                url: '{{ route('task.store') }}',
+                method: 'POST',
+                data: {
+                    "_token": "{{ csrf_token() }}",
+                    "title": title.val(),
+                    "description": desc.val(),
+                    "deadline_date": date.val(),
+                    "user_id": user.val(),
+                },
                 success: data => {
+                    $('#TaskCreate').modal('hide');
                     console.log(data);
+                    let result = $('#tasks-scroll').append(data.view).show('slide', {direction: 'left'}, 400);
                 },
                 error: () => {
-                    console.log('error');
+                    console.log(0);
                 }
             })
-        </script>
-        <script>
-            $('.addTask').click(e => {
-                e.preventDefault();
-                let btn = $(e.currentTarget);
-                let title = $('#taskname');
-                let desc = $('#taskdescription');
-                let date = $('#taskdate');
-                let user = $('#taskuser');
-
-                $.ajax({
-                    url: '{{ route('task.store') }}',
-                    method: 'POST',
-                    data: {
-                        "_token": "{{ csrf_token() }}",
-                        "title": title.val(),
-                        "description": desc.val(),
-                        "deadline_date": date.val(),
-                        "user_id": user.val(),
-                    },
-                    success: data => {
-                        $('#TaskCreate').modal('hide');
-                        console.log(data);
-                        let result = $('#tasks-scroll').append(data.view).show('slide', {direction: 'left'}, 400);
-                    },
-                    error: () => {
-                        console.log(0);
-                    }
-                })
-            })
-        </script>
-        <script>
-            $('.call-btn').click(function (e) {
+        })
+    </script>
+    <script>
+        function registerCallBtn(item) {
+            item.click(function (e) {
                 e.preventDefault();
                 let btn = $(e.currentTarget);
                 let id = btn.data('id');
@@ -78,57 +78,61 @@
                 let href = btn.attr('href');
                 window.location.href = href;
             });
-        </script>
-        <script>
-            $('.call_add').click(function (e) {
-                e.preventDefault();
-                let btn = $(e.currentTarget);
-                let id = $('#caller_id').val();
-                $.ajax({
-                    url: '{{ route('call_to_customer') }}',
-                    method: 'POST',
-                    data: {
-                        "_token": "{{ csrf_token() }}",
-                        "id": id,
-                    },
-                    success: data => {
-                        $('#calledModal').modal('hide');
-                        console.log(data);
-                        let result = $('#customers-scroll').append(data.view).show('slide', {direction: 'left'}, 400);
-                    },
-                    error: () => {
-                        console.log(0);
-                    }
-                })
-            })
-        </script>
-        <script>
-            $('.addCall').click(e => {
-                e.preventDefault();
-                let btn = $(e.currentTarget);
-                let excel = $('#excel')[0].files[0];
-
-                var formData = new FormData();
-                formData.append('_token', "{{ csrf_token() }}");
-                if (excel != undefined){
-                    formData.append('excel', excel);
+        }
+    </script>
+    <script>
+        $('.call_add').click(function (e) {
+            e.preventDefault();
+            let btn = $(e.currentTarget);
+            let id = $('#caller_id').val();
+            $.ajax({
+                url: '{{ route('call_to_customer') }}',
+                method: 'POST',
+                data: {
+                    "_token": "{{ csrf_token() }}",
+                    "id": id,
+                },
+                success: data => {
+                    $('#calledModal').modal('hide');
+                    console.log(data);
+                    let result = $('#customers-scroll').append(data.view).show('slide', {direction: 'left'}, 400);
+                },
+                error: () => {
+                    console.log(0);
                 }
-                $.ajax({
-                    url: '{{ route('excel.import') }}',
-                    method: 'POST',
-                    processData: false,
-                    contentType: false,
-                    data: formData,
-                    success: data => {
-                        $('#CallCreate').modal('hide');
-                        console.log(data);
-                        let result = $('#calls-scroll').append(data.view).show('slide', {direction: 'left'}, 400);
-                    },
-                    error: () => {
-                        console.log(0);
-                    }
-                })
             })
-        </script>
-    @endpush
+        })
+    </script>
+    <script>
+        $('.addCall').click(e => {
+            e.preventDefault();
+            let btn = $(e.currentTarget);
+            let excel = $('#excel')[0].files[0];
+
+            var formData = new FormData();
+            formData.append('_token', "{{ csrf_token() }}");
+            if (excel != undefined) {
+                formData.append('excel', excel);
+            }
+            $.ajax({
+                url: '{{ route('excel.import') }}',
+                method: 'POST',
+                processData: false,
+                contentType: false,
+                data: formData,
+                success: data => {
+                    $('#CallCreate').modal('hide');
+                    console.log(data);
+                    let result = $('#calls-scroll').append(data.view).show('slide', {direction: 'left'}, 400);
+                    result.find('.call-btn').each((e, i) => {
+                        registerCallBtn($(i));
+                    });
+                },
+                error: () => {
+                    console.log(0);
+                }
+            })
+        })
+        registerCallBtn($('.call-btn'));
+    </script>
 @endpush
