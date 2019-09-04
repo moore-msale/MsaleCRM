d6f08e2cb4abeede78315bb4c15485e64307065e
Erkin <mackinkenny@gmail.com>
2019-09-04 21:47:00 +0600

    df

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -29,13 +29,13 @@
 <body>
     <div id="app">
         @include('_partials.header')
-        <main>
+        <main >
             <div class="container-fluid">
                 <div class="row ">
                     <div class="col-auto px-0 d-lg-block d-none" style="width:5%;">
                         @include('_partials.sidebar')
                     </div>
-                    <div class="col-lg-14 col-15mt-5">
+                    <div class="col-lg-14 col-15 pt-5">
                         @yield('content')
                     </div>
                 </div>
---
+++ resources/views/layouts/app2.blade.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ config('app.name', 'Laravel') }}</title>
+
+    <!-- Scripts -->
+
+
+    <!-- Fonts -->
+    <link rel="dns-prefetch" href="//fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.1/css/all.css">
+
+    <!-- Styles -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <script src="https://code.jquery.com/jquery-1.12.3.min.js" integrity="sha256-aaODHAgvwQW1bFOGXMeX+pC4PZIPsvn2h1sArYOhgXQ=" crossorigin="anonymous"></script>
+    <script type="text/javascript" src="http://momentjs.com/downloads/moment-with-locales.min.js"></script>
+    <link rel="stylesheet" href="{{ asset('css/bootstrap-material-datetimepicker.css') }}" />
+    <link href="{{ asset('css/main.css') }}" rel="stylesheet">
+    @stack('styles')
+</head>
+<body>
+<div id="app">
+
+    <main>
+        <div class="container-fluid">
+            <div class="row ">
+                <div class="col-auto px-0 d-lg-block d-none" style="width:5%;">
+
+                </div>
+                <div class="col-lg-15 col-15 mt-5">
+                    @yield('content')
+                </div>
+            </div>
+        </div>
+
+    </main>
+</div>
+
+<script src="{{ asset('js/app.js') }}"></script>
+<script type="text/javascript" src="{{ asset('js/bootstrap-material-datetimepicker.js') }}"></script>
+<script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
+
+</body>
+</html>
