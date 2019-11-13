9938a6241b52394326c48612cefd1f10328c49d9
Erkin <mackinkenny@gmail.com>
2019-11-13 10:42:55 +0600

    dee

--- app/Http/Controllers/AjaxController.php
+++ app/Http/Controllers/AjaxController.php
@@ -11,15 +11,18 @@ class AjaxController extends Controller
     public function searchTask(Request $request)
     {
         $search = $request->search;
-        $result = collect(['Задачи' => Task::where('title', 'like', "%$search%")->where('taskable_type', null)->get()]);
+        $result = collect(['Задачи' => Task::where('taskable_type', null)->where('title', 'like', "%$search%")->orWhere('description','like',"%$search%")->orWhere('deadline_date','like',"%$search%")->get()]);
+        $count = count($result);
 
         if ($request->ajax()) {
             return response()->json([
                 'html' => view('_partials.search_result', [
                     'result' => $result,
+                    'count' => $count,
                 ])->render(),
             ]);
         }
+
         return view('_partials.search-result', [
             'result' => $result,
         ]);
