8432ca4a1e54a0ebd91646a3341971f321a757be
Ormonali <kaarov8@gmail.com>
2019-12-06 16:07:07 +0600

    excel import only if have two values

--- app/Http/Controllers/NotificationController.php
+++ app/Http/Controllers/NotificationController.php
@@ -25,7 +25,7 @@ class NotificationController extends Controller
         $now = Carbon::now();
         $next = Carbon::now()->addHours(2);
         $tasks = Task::all();
-
+        $admin = User::find(1);
         foreach ($tasks as $task)
         {
             if($task->deadline_date > $now && $task->deadline_date < $next)
@@ -46,12 +46,14 @@ class NotificationController extends Controller
                 $task->save();
                 }
                 }
+            }elseif($task->deadline_date > $next){
+                 $task->status_id = null;
             }
             if($task->taskable_type == null){
                 if($task->status_id != 1 && $task->status_id != 2 && User::find($task->user_id)->role != 'admin' && $task->deadline_date < $now && $task->chief == 1)
                 {
                     $user = User::find($task->user_id);
-                    $user->balance = $user->balance - 200;
+                    $user->balance = $user->balance - isset($user->penalty) ? $user->penalty : $admin->penalty;
                     $task->status_id = 2;
                     $task->chief = 2;
                     $user->save();
@@ -68,28 +70,7 @@ class NotificationController extends Controller
                     Mail::to($user->email)->send(new TaskPenaltyByChief($task));
                     Mail::to('buvladi@gmail.com')->send(new PenaltyNotificationToChief($task));
                 }
-            }elseif($task->taskable_type == 'App\Customer'){
-            if($task->status_id != 1 && $task->status_id != 2 && User::find($task->user_id)->role != 'admin' && $task->deadline_date < $now && $task->chief == 1)
-            {
-                $user = User::find($task->user_id);
-                $user->balance = $user->balance - 200;
-                $task->status_id = 2;
-                $task->chief = 2;
-                $user->save();
-                $task->save();
-                Mail::to($user->email)->send(new TaskPenaltyByChief($task));
-                Mail::to('buvladi@gmail.com')->send(new PenaltyNotificationToChief($task));
             }
-            elseif($task->status_id != 1 && $task->status_id !=2 && User::find($task->user_id)->role != 'admin' && $task->deadline_date < $now)
-            {
-                $user = User::find($task->user_id);
-                $task->status_id = 2;
-                $task->save();
-                $user->save();
-                Mail::to($user->email)->send(new TaskPenaltyByChief($task));
-                Mail::to('buvladi@gmail.com')->send(new PenaltyNotificationToChief($task));
-            }
-        }
 
         }
     }
